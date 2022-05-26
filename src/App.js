d56ba9c38e5b7078e8c9211970064c9da7f7fe17
ShubhamShrestha25 <shubhamshrestha25@gmail.com>
2022-05-26 19:45:52 +0545

    1st commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <CartContainer />
+    </div>
+  );
+}
+export default App;
